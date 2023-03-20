c3c83e9f06ba88dc55ddc37fda58b788ed54701c
wetsj <wetsjunior@gmail.com>
2023-03-20 15:43:44 +0000

    update dashboard and navbar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,20 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-2">
-            <ul class="list-group">
-                <a class="list-group-item list-group-item-action active" aria-current="true" href="#">Questions</a>
+<div class="container-fluid">
+    <div class="row ">
+    
+            <div class="col-3 border-end" style="height:125px;">
+                <ul class="list-group list-group-flush">
+                    <a class="list-group-item list-group-item-action active" aria-current="true" href="#"><i class="bi bi-globe"></i>&ensp;Toutes les questions</a>
+                    
                 
-              
-                <a class="list-group-item list-group-item-action" aria-current="false" href="#">Tags</a>
-                
-                
-                <a class="list-group-item list-group-item-action" aria-current="false" href="#">Utilisateurs</a>
-                
-            </ul>
-        </div>
+                    <a class="list-group-item list-group-item-action" aria-current="false" href="#"><i class="bi bi-tags"></i>&ensp;Tags</a>
+                    
+                    
+                    <a class="list-group-item list-group-item-action" aria-current="false" href="#"><i class="bi bi-person"></i>&ensp;Utilisateurs</a>
+                    
+                </ul>
+            </div>
+        
         <div class="col-8">
             <div class="title d-flex justify-content-between mb-3"><h3>All questions</h3>
                 <button type="submit" class="btn btn-primary">Poser une question</button>
@@ -30,7 +32,10 @@
             <div class="card w-100 mb-3">
                 <div class="card-body">
                     <h5 class="card-title">Card title</h5>
-                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    
+                            <p class="card-text ">With supporting text below as a natural lead-in to additional content.</p>
+                       
+                    
                     <span class="badge bg-primary">PHP</span>
                 </div>
             </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link rel="stylesheet" href="./resources/css/app.css">
+    <link rel="stylesheet" href="css/app.css">
     <!-- BootStrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
@@ -27,69 +27,31 @@
 <body>
     <div id="app">
         <!-- Navbar -->
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm">
-            <!-- Container wrapper -->
-            <div class="container">
-                <!-- Navbar brand -->
-                <a class="navbar-brand" href="#"><img id="MDB-logo" src="https://mdbcdn.b-cdn.net/wp-content/uploads/2018/06/logo-mdb-jquery-small.png" alt="MDB Logo" draggable="false" height="30" /></a>
-
-                <!-- Toggle button -->
-                <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <i class="fas fa-bars"></i>
-                </button>
-
-                <!-- Collapsible wrapper -->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm d-flex justify-content-between">
+            
+            <div class="container d-flex">
+                
+                <a class="navbar-brand col-2" href="#"><i heigth="30" class="bi bi-stack-overflow"> Q&Rflow</i></a>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left links -->
-                    <ul class="navbar-nav me-3">
-                        <li class="nav-item">
-                            <a class="nav-link active d-flex align-items-center" aria-current="page" href="#"><i class="fas fa-bars pe-2"></i>Menu</a>
-                        </li>
-                    </ul>
-                    <!-- Left links -->
-
-                    <form class="d-flex align-items-center w-100 form-search">
+                    
+                    <form class="d-flex align-items-center w-100 form-search ">
                         <div class="input-group">
-                            <button class="btn btn-light dropdown-toggle shadow-0" type="button" data-mdb-toggle="dropdown" aria-expanded="false" style="padding-bottom: 0.4rem;">
-                                Catégories
-                            </button>
-                            <ul class="dropdown-menu dropdown-menu-dark fa-ul">
-                                <li>
-                                    <a class="dropdown-item" href="#"><span class="fa-li pe-2"><i class="fa fa-search"></i></span>All</a>
-                                </li>
-                                <li>
-                                    <a class="dropdown-item" href="#"><span class="fa-li pe-2"><i class="fas fa-film"></i></span>Titles</a>
-                                </li>
-                                <li>
-                                    <a class="dropdown-item" href="#"><span class="fa-li pe-2"><i class="fa fa-tv"></i></span>TV
-                                        Episodes</a>
-                                </li>
-                                <li>
-                                    <a class="dropdown-item" href="#"><span class="fa-li pe-2"><i class="fas fa-user-friends"></i></span>Celebs</a>
-                                </li>
-                                <li>
-                                    <a class="dropdown-item" href="#"><span class="fa-li pe-2"><i class="fas fa-building"></i></span>Companies</a>
-                                </li>
-                                <li>
-                                    <a class="dropdown-item" href="#"><span class="fa-li pe-2"><i class="fas fa-key"></i></span>Keywords</a>
-                                </li>
-                            </ul>
-                            <input type="search" class="form-control" placeholder="Search" aria-label="Search" />
+                            <input type="search" class="form-control" placeholder="Rechercher" aria-label="Search" />
                         </div>
                         <a href="#!" class="text-white"><i class="fas fa-search ps-3"></i></a>
                     </form>
 
-                    <ul class="navbar-nav ms-3">
+                    <ul class="navbar-nav ms-3 d-flex">
                         @guest
                         @if (Route::has('login'))
                         <li class="nav-item ">
-                            <a class="nav-link d-flex align-items-center me-3" href="{{ route('login') }}">{{ __('Connexion') }}</a>
+                            <a style="font-size: 16px;" class="nav-link align-items-center me-3" href="{{ route('login') }}">{{ __('Connexion') }}</a>
                         </li>
                         @endif
 
                         @if (Route::has('register'))
                         <li class="nav-item" style="width: 65px;">
-                            <a class="nav-link d-flex align-items-center" href="{{ route('register') }}">{{ __('Inscription') }}</a>
+                            <a style="font-size: 16px;" class="nav-link  align-items-center" href="{{ route('register') }}">{{ __('Inscription') }}</a>
                         </li>
                         @endif
                         @else
@@ -118,7 +80,7 @@
         </nav>
         <!-- Navbar -->
 
-        <main class="py-4">
+        <main class="py-3">
             @yield('content')
         </main>
     </div>
